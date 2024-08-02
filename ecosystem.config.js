d36c573ce65d92aff6221eac77374bd466601e47
dikshya-bhandari <dikshyabhandari05@gmail.com>
2024-08-01 19:34:20 -0500

    Update Readme.md file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,14 +12,14 @@ module.exports = {
       ignore_watch: ["node_modules", "assets", "docs", ".git"],
       max_memory_restart: process.env.APP_MEMORY_LIMIT || "2G",
     },
-    // {
-    //   name: "covidanalysis.worker",
-    //   script: "./worker.js",
-    //   autorestart: true,
-    //   // watch: process.env.NODE_ENV == 'development' ? ['src', '*.js', '*.ts', '.env'] : false,
-    //   watch_delay: 5000,
-    //   ignore_watch: ["node_modules", "assets", "docs", ".git"],
-    //   max_memory_restart: process.env.APP_MEMORY_LIMIT || "2G",
-    // },
+    {
+      name: "covidanalysis.worker",
+      script: "./worker.js",
+      autorestart: true,
+      // watch: process.env.NODE_ENV == 'development' ? ['src', '*.js', '*.ts', '.env'] : false,
+      watch_delay: 5000,
+      ignore_watch: ["node_modules", "assets", "docs", ".git"],
+      max_memory_restart: process.env.APP_MEMORY_LIMIT || "2G",
+    },
   ],
 };
